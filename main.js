cc23420cb73e0c1e5c28b14001623b992524f875
dylan <7257923+dmnuggins@users.noreply.github.com>
2023-08-01 18:43:24 -0700

    Set up basic app layout

---
+++ main.js
@@ -0,0 +1,14 @@
+let library = []
+const DEFAULT_DATA = [
+  {
+    name: 'Discourses & Selected Writings',
+    author: 'Epictetus',
+    status: 'read',
+  },
+  {
+    name: 'Alice in Wonderland',
+    author: 'Lewis Caroll',
+    status: 'not read',
+  },
+  { name: 'Naruto', author: 'Masashi Kishimoto', status: 'read' },
+]
